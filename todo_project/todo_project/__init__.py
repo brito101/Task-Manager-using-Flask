1258599f6aafeeacfffe4b5d8fcf218b0a3babac
brito101 <rodrigobrito101@hotmail.com>
2026-06-13 09:45:53 -0300

    first commit

---
+++ todo_project/todo_project/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///site.db')
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' 
+login_manager.login_message_category = 'danger'
+
+bcrypt = Bcrypt(app)
+
+# Always put Routes at end
+from todo_project import routes
+
+with app.app_context():
+    db.create_all()
